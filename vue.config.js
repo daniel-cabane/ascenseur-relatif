041f5384102c81df87109dad12c393811fa3640f
Daniel <daniel.cabane@gmail.com>
2020-08-22 00:18:24 +0800

    Redeploy !

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,7 @@ module.exports = {
   "transpileDependencies": [
     "vuetify"
   ],
+  outputDir: "docs",
   publicPath: process.env.NODE_ENV === 'production'
     ? '/ascenseur-relatif/'
     : '/'
