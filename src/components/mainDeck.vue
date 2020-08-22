efaf999c71b246457be7130a92f72ad1c3501e8e
Daniel <daniel.cabane@gmail.com>
2020-08-22 20:26:05 +0800

    added challenges

--- src/components/mainDeck.vue
+++ src/components/mainDeck.vue
@@ -1,5 +1,16 @@
 <template>
-  <v-container>
+  <v-container @keydown.enter='submitAnswer'>
+    <v-alert type="error" v-if='["xs", "sm"].includes(this.$vuetify.breakpoint.name)'>
+      Votre écran n'est pas assez large pour afficher cette application correctement
+    </v-alert>
+    <v-snackbar v-model="snackbar" top :color='snackColor'>
+      {{ snackText }}
+      <template v-slot:action="{ attrs }">
+        <v-btn :color="snackColor" text v-bind="attrs" @click="snackbar = false">
+          Close
+        </v-btn>
+      </template>
+    </v-snackbar>
     <v-row>
       <v-col cols='9'>
         <v-row>
@@ -10,11 +21,11 @@
             <v-hover v-slot:default="{ hover }">
               <v-card
                 class='display-4 font-weight-bold'
-                :elevation="hover ? 5 : 0"
+                :elevation="hover && !challengeActive ? 5 : 0"
                 :color="hover ? 'white' : 'white'"
                 height="80" width="80"
                 style='display:flex;justify-content:center;align-items:center;cursor:pointer'
-                @click='operation = !operation'
+                @click='switchOperation'
                 >
                 <span v-if='operation'>+</span>
                 <span v-else>–</span>
@@ -25,13 +36,13 @@
             <tokenPanel :index='1'/>
           </v-col>
         </v-row>
-        <v-btn block color='primary' x-large class='mt-4' @click='moveLift'>
+        <v-btn block color='primary' x-large class='mt-4' @click='moveLift' :disabled='challengeActive'>
           Effectuer
         </v-btn>
       </v-col>
       <v-col cols='1'></v-col>
       <v-col cols='2' style='position:relative' class='py-0'>
-          <v-card x-large color="primary" outlined class='display-1 font-weight-bold text-xs-center white--text' min-width='120px' max-width='120px'>
+          <v-card x-large color="primary" outlined class='display-1 font-weight-bold text-xs-center white--text' style='z-index:10' min-width='120px' max-width='120px'>
             <div style='width:100%;display:flex;justify-content:center'>
               {{ result | formatNb }}
             </div>
@@ -39,6 +50,26 @@
         <v-img min-width='170px' max-width='170px' src="../assets/axe.png"></v-img>
         <v-img id='lift' class='smoothMove' min-width='75px' max-width='75px' src="../assets/lift.png" style='position:absolute;top:219px;left:34px'></v-img>
       </v-col>
+    </v-row>
+    <v-row class='mt-4'>
+      <v-select v-model='challengeNb' :items="challenges" class='mx-2' label="Sélectionner un défi" :disabled='challengeActive' outlined></v-select>
+      <div style='display:flex' v-if='challengeActive'>
+        <v-text-field type='number' v-model='challengeResult' label='Résultat' autofocus outlined v-if='challengeNb%2 == 1'/>
+        <v-btn class="mx-2" fab dark color="success" @click='submitAnswer'>
+          <v-icon dark>done</v-icon>
+        </v-btn>
+        <v-btn class="mx-2" fab dark color="error" @click='endChallenge'>
+          <v-icon dark>clear</v-icon>
+        </v-btn>
+      </div>
+      <v-btn style='width:215px' x-large class='mx-2' color='primary' @click='startChallenge' v-else>
+        Lancer le défi
+      </v-btn>
+    </v-row>
+    <v-row class='headline mb-4'>
+      <v-spacer></v-spacer>
+      <span v-html='challengeText'></span>
+      <v-spacer></v-spacer>
     </v-row>
   </v-container>
 </template>
@@ -55,6 +86,20 @@
       operation: true,
       terms: [0, 0],
       result: 0,
+      challenges: [
+        {text: 'Défi 1 - Addition', value: 1},
+        {text: 'Défi 2 - Addition incomplète', value: 2},
+        {text: 'Défi 3 - Soustraction', value: 3},
+        {text: 'Défi 4 - Soustraction incomplète', value: 4},
+      ],
+      challengeNb: null,
+      challengeResult: '',
+      challengeActive: false,
+      challengeText: '',
+      expectedResult: 0,
+      snackbar: false,
+      snackColor: 'success',
+      snackText: 'Exact !'
     }),
     mounted(){
       this.eventBus.$on('emitTerm', data => {
@@ -71,20 +116,72 @@
     methods: {
       moveLift(){
         let result = this.operation ? parseInt(this.terms[0])+parseInt(this.terms[1]) : parseInt(this.terms[0])-parseInt(this.terms[1]);
-        document.getElementById("lift").style.transform = `translateY(${-result*12}px)`;
+        document.getElementById("lift").style.transform = `translateY(${-result*12+1+Math.sign(result)}px)`;
         if(this.result != result){
           let count = setInterval(()=> {
             this.result += Math.sign(result-this.result);
             if(this.result == result) clearInterval(count);
           }, 500/Math.abs(result-this.result));
         }
+      },
+      switchOperation(){
+        if(!this.challengeActive){
+          this.operation = !this.operation;
+        }
+      },
+      startChallenge(){
+        if(this.challengeNb > 0){
+          document.getElementById("lift").style.transform = 'translateY(0px)';
+          if(this.result != 0){
+            let count = setInterval(()=> {
+              this.result += Math.sign(-this.result);
+              if(this.result == 0) clearInterval(count);
+            }, 500/Math.abs(-this.result));
+          }
+          this.challengeResult = '';
+          this.eventBus.$emit('startChallenge', this.challengeNb);
+          this.operation = this.challengeNb <= 2;
+          this.challengeActive = true;
+          switch(this.challengeNb%2){
+            case 0:
+            this.expectedResult = Math.sign(Math.random()-0.5)*Math.floor(Math.random()*15);
+            this.challengeText = `Complète l'opération pour arriver à l'étage <span class='display-1 font-weight-bold'>${this.expectedResult}</span>`
+            break;
+            case 1:
+              this.challengeText = "Effectue l'opération"
+            break;
+          }
+        }
+      },
+      submitAnswer(){
+        if(this.challengeActive){
+          let resultA = this.operation ? parseInt(this.terms[0])+parseInt(this.terms[1]) : parseInt(this.terms[0])-parseInt(this.terms[1]);
+          let resultB = this.challengeNb%2 == 0 ? this.expectedResult : this.challengeResult
+          if(resultA == resultB){
+            this.snackColor = 'success';
+            this.snackText = 'Exact !';
+            this.snackbar = true;
+            this.endChallenge();
+          } else {
+            this.challengeResult = '';
+            this.snackColor = 'error';
+            this.snackText = 'Non, essaie encore';
+            this.snackbar = true;
+          }
+        }
+      },
+      endChallenge(){
+        this.moveLift();
+        this.challengeActive = false;
+        this.challengeText = '';
+        this.eventBus.$emit('endChallenge', {});
       }
     }
   }
 </script>
 
 <style scoped>
-.smoothMove {
-  transition: all .5s
-}
+  .smoothMove {
+    transition: all .5s
+  }
 </style>
--- src/components/tokenPanel.vue
+++ src/components/tokenPanel.vue
@@ -1,21 +1,24 @@
 <template>
   <v-row>
-    <v-card color='grey lighten-4' width='100%' min-width='370px' height='400px' class='pa-2 mb-2'>
-      <span class='inset-text'>{{ value }}</span>
+    <v-card color='grey lighten-4' width='100%' min-width='280px' height='400px' class='pa-2 mb-2'>
+      <span class='inset-text' :style='["lg", "xl"].includes(this.$vuetify.breakpoint.name) ? "font-size:200px" : "font-size:150px"'>{{ value }}</span>
       <v-avatar v-for="(token, index) in tokenList" :key='`token-${index}`' :color="token ? 'white' : 'black'" class='ma-1 elevation-4'>
         <span class="display-1" v-if='token'>+</span>
         <span class="display-1 white--text" v-else>–</span>
       </v-avatar>
+      <v-card color='error' class='white--text pa-2' style='position:absolute;right:5px;bottom:5px' v-if='locked'>
+        <v-icon color='white' medium>lock</v-icon>
+      </v-card>
       <span></span>
     </v-card>
     <v-row style='display:flex;justify-content:space-around'>
-      <v-btn @click='addWhite' :disabled='tokenList.length >= 20'>
-        add <v-icon right>add_circle_outline</v-icon>
+      <v-btn @click='addWhite' :disabled='tokenList.length >= 20 || locked'>
+        Jeton <v-icon right>add_circle_outline</v-icon>
       </v-btn>
-      <v-btn @click='addBlack' :disabled='tokenList.length >= 20'>
-        add <v-icon right>remove_circle</v-icon>
+      <v-btn @click='addBlack' :disabled='tokenList.length >= 20 || locked'>
+        Jeton <v-icon right>remove_circle</v-icon>
       </v-btn>
-      <v-btn @click='clear' color='error' outlined>
+      <v-btn @click='clear' color='error' outlined :disabled='locked'>
         clear <v-icon right>clear</v-icon>
       </v-btn>
     </v-row>
@@ -27,7 +30,8 @@
     name: 'tokenPanel',
     props: { index: Number },
     data: () => ({
-      tokenList: []
+      tokenList: [],
+      locked: false
     }),
     computed: {
       value(){
@@ -39,21 +43,50 @@
         return value > 0 ? `+${value}` : value;
       }
     },
+    mounted(){
+      this.eventBus.$on('startChallenge', nb => {
+        switch(nb%2){
+          case 0:
+          if(this.index == 0){
+            this.randomTokens();
+          } else {
+            this.tokenList = [];
+          }
+          break;
+          case 1:
+            this.randomTokens();
+          break;
+        }
+      });
+      this.eventBus.$on('endChallenge', () => {
+        this.locked = false;
+      });
+    },
     methods: {
       addWhite(){
-        if(this.tokenList.length < 20){
+        if(this.tokenList.length < 25){
           this.tokenList.push(true);
           this.eventBus.$emit('emitTerm', {val: this.value, index: this.index});
         }
       },
       addBlack(){
-        if(this.tokenList.length < 20){
+        if(this.tokenList.length < 25){
           this.tokenList.push(false);
           this.eventBus.$emit('emitTerm', {val: this.value, index: this.index});
         }
       },
       clear(){
         this.tokenList = [];
+        this.eventBus.$emit('emitTerm', {val: this.value, index: this.index});
+      },
+      randomTokens(){
+        let tokenValue = Math.random() < 0.5;
+        let nbTokens = Math.floor(Math.random()*16);
+        this.tokenList = [];
+        for(let i=0; i<nbTokens ; i++){
+          this.tokenList.push(tokenValue);
+        }
+        this.locked = true;
         this.eventBus.$emit('emitTerm', {val: this.value, index: this.index});
       }
     }
@@ -70,7 +103,7 @@
     display: flex;
     justify-content: center;
     align-items: center;
-    font-size: 200px;
+
     color: rgba(240,240,240, 0.6);
     text-shadow: 1px 4px 6px #eee, 0 0 0 #333, 1px 4px 6px #eee;
 }
